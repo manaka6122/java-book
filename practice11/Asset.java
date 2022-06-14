568ff7c509177d7b46e6c91e1d09090ea4fdb7a8
松本愛可 <matsumotomanaka@matsumoikanoAir.sensyu.ne.jp>
2022-06-14 16:25:40 +0900

    Review of conditional branching and iterative processing

--- practice11/Asset.java
+++ practice11/Asset.java
@@ -5,7 +5,7 @@ public abstract class Asset {
   public Asset(String name, int price) {
     this.name = name;
     this.price = price;
-  }
+  } 
   public String getName() { return this.name; }
   public int hetPrice() { return this.price; }
 }
